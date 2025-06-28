eb8c9ae178cc3f5c3bfd3a515719ab96c15e170f
Jomer Ubaldo <Jomerubaldo@gmail.com>
2025-06-28 16:52:11 +0800

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
-let $name: string = "Practice";
-console.log($name);
+let name1: string;
+
+let age: number;
+
+let isStudent: boolean;
+
+let subjects: string[];
+
+let position: [number, string];
+
+position = [12, "Jomer"];
+
+type Student = {
+  name: string;
+  age?: number;
+};
+
+let student1: Student = {
+  name: "Jomer",
+};
+
+let lotsOfStudents: string;
